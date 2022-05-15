ed08f88da03e56e4dfa5be47be1c9e6e52b485a6
Romanenkov Dmitriy <215rd@mail.ru>
2022-05-15 20:03:33 +0300

    feat: full refactor all code, try to use async/await, .then

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,47 +1,59 @@
 const fs = require('fs');
 const path = require('path');
+const fsp = require('fs/promises');
+
+
+async function createIndexHTML() {
+  try {
+    await fsp.unlink('./06-build-page/project-dist/index.html');
+  } catch(error) {
+    console.log('Существующий index.js не найден. Создаем новый.');
+  }
+
+  try {
+    await fsp.rmdir('./06-build-page/project-dist/');
+  } catch {
+    console.log('Существующая папка project-dist не найдена. Создаем новую.');
+  }
+
+  await fsp.mkdir(path.join(__dirname, 'project-dist'))
+    .then(fsp.copyFile('./06-build-page/template.html', './06-build-page/project-dist/index.html'))
+    .catch(error => console.log(error.code));
+}
 
 
 const components = {};
 
+async function createComponents() {
+  await fsp.readdir(path.join(__dirname, 'components'))
+    .then((data) => data.forEach((item) => {
+      if (path.extname(item) === '.html') {
+        createComp(item);
+      }
+    }));
+}
 
-fs.readdir(path.join(__dirname, 'components'), (error, data) => {
-  data.forEach((item) => {
-    fs.readFile(path.join(__dirname, 'components', item), (error, data) => {
-      components[`${item}`] = data.toString();
-      createIndex();
+async function createComp(item) {
+  await fsp.readFile(path.join(__dirname, 'components', item))
+    .then((data) => {
+      components[`${item.split('.')[0]}`] = data.toString();
     });
-  });
-});
+}
 
-const createIndex = () => {
-  fs.readFile('./06-build-page/project-dist/index.html', (error, data) => {
-    Object.keys(components).forEach((item) => {
-      item = item.split('.')[0];
-      console.log(item);
-      let reg = RegExp(`{{${item}}}`);
-      console.log(reg);
-      console.log(components);
-      data = data.toString().replace(reg, components[`${item}.html`]);
-      fs.writeFile('./06-build-page/project-dist/index.html', data, () => {});
-    });
-    console.log(data);
-/*     data = data.toString().replace(/{{/g, path.join(__dirname, 'components'));
-    data = data.toString().replace(/}}/g, '.html');
-    console.log(data.toString()); */
-    
+async function template(comp) {
+  let htmlText = await fsp.readFile('./06-build-page/project-dist/index.html');
+  htmlText = htmlText.toString();
+  Object.keys(comp).forEach((item) => {
+    let reg = RegExp(`{{${item}}}`);
+    htmlText = htmlText.replace(reg, components[`${item}`]);
   });
-};
+  await fsp.writeFile('./06-build-page/project-dist/index.html', htmlText);
+}
 
-const createPage = () => {
-  fs.mkdir(path.join(__dirname, 'project-dist'), () => {
-  });
-  fs.copyFile('./06-build-page/template.html', './06-build-page/project-dist/index.html', (error, data) => {
-/*     let readStream = fs.createReadStream('./06-build-page/project-dist/index.html');
-    let writeStream = fs.createWriteStream('./06-build-page/project-dist/index.html');
-    readStream.on('data', (chunk) => {
-    }) */
-  });
-};
+async function creator() {
+  await createIndexHTML();
+  await createComponents();
+  await template(components);
+}
 
-createPage();
+creator();
