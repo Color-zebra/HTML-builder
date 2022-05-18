7fbc4946603d5f0293573a82c014905f3772409f
Romanenkov Dmitriy <215rd@mail.ru>
2022-05-18 19:26:05 +0300

    refactor: refactor third task with async/await

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,17 @@
-const fs = require('fs');
 const path = require('path');
+const fsp = require('fs/promises');
 
-fs.readdir('03-files-in-folder/secret-folder',{withFileTypes: true}, (error, data) => {
-  if (error) console.log(error);
-  let allFiles = data;
-  allFiles.forEach((item) => {
-    if(item.isFile() === true) {
-      let filePath = path.join(__dirname, 'secret-folder', item.name);
-      let fileExtension = path.extname(filePath);
-      let fileName = path.basename(filePath, path.extname(filePath));
-      fs.stat(filePath, (error, date) => {
-        if (error) console.log(error);
-        console.log(fileName + ' - ' + fileExtension.slice(1) + ' - ' + date.size/1000 + 'kb');
-      });
+const pathTo = path.join(__dirname, 'secret-folder');
+
+const filesInFolder = async (pathTo) => {
+  let allFilesInFolder = await fsp.readdir(pathTo, {withFileTypes: true});
+  for (let file of allFilesInFolder) {
+    if (file.isFile()) {
+      let slicedFileName = file.name.split('.');
+      let fileStat = await fsp.stat(path.join(pathTo, file.name));
+      console.log(slicedFileName[0], '-', slicedFileName[slicedFileName.length-1], '-', fileStat.size + ' bytes');
     }
-  });
-});
+  }
+};
+
+filesInFolder(pathTo);
